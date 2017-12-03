56b9b2a3fcb66899a6675243f8fc69e9a24169b7
ajlopez <webmaster@ajlopez.com>
2017-12-03 19:19:16 -0300

    Compile swap bytecodes

--- test/compilers/java.js
+++ test/compilers/java.js
@@ -1,5 +1,6 @@
 
 var compilers = require('../../lib/compilers/java');
+var hexas = "0123456789abcdef";
 
 exports['create compiler as object'] = function (test) {
 	var compiler = compilers.compiler();
@@ -17,5 +18,31 @@ exports['compile dup1 bytecode'] = function (test) {
 	test.ok(Array.isArray(result));
 	test.equal(result.length, 1);
 	test.equal(result[0], 'dup(1);');
+}
+
+exports['compile dup bytecodes'] = function (test) {
+	var compiler = compilers.compiler();
+	
+	for (var k = 0; k < 16; k++) {
+		var result = compiler.compile('8' + hexas[k]);
+		
+		test.ok(result);
+		test.ok(Array.isArray(result));
+		test.equal(result.length, 1);
+		test.equal(result[0], 'dup(' + (k + 1) + ');');
+	}
+}
+
+exports['compile swap bytecodes'] = function (test) {
+	var compiler = compilers.compiler();
+	
+	for (var k = 0; k < 16; k++) {
+		var result = compiler.compile('9' + hexas[k]);
+		
+		test.ok(result);
+		test.ok(Array.isArray(result));
+		test.equal(result.length, 1);
+		test.equal(result[0], 'swap(' + (k + 1) + ');');
+	}
 }
 
