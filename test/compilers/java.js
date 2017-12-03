994bc5173f7cd50d25da06836b7c39ba3cdd4866
ajlopez <webmaster@ajlopez.com>
2017-12-03 19:11:02 -0300

    Compile bytecode DUP1 to Java

--- test/compilers/java.js
+++ test/compilers/java.js
@@ -7,3 +7,15 @@ exports['create compiler as object'] = function (test) {
 	test.ok(compiler);
 	test.equal(typeof compiler, 'object');
 }
+
+exports['compile dup1 bytecode'] = function (test) {
+	var compiler = compilers.compiler();
+	
+	var result = compiler.compile('80');
+	
+	test.ok(result);
+	test.ok(Array.isArray(result));
+	test.equal(result.length, 1);
+	test.equal(result[0], 'dup(1);');
+}
+
