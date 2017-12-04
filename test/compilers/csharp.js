ec4a0b3d960320ce83838fd4bef88298449982db
ajlopez <webmaster@ajlopez.com>
2017-12-03 21:34:24 -0300

    Compiling to simple c#@

---
+++ test/compilers/csharp.js
@@ -0,0 +1,96 @@
+
+var compilers = require('../../lib/compilers/csharp');
+var hexas = "0123456789abcdef";
+
+exports['create compiler as object'] = function (test) {
+	var compiler = compilers.compiler();
+	
+	test.ok(compiler);
+	test.equal(typeof compiler, 'object');
+}
+
+exports['compile dup1 bytecode'] = function (test) {
+	var compiler = compilers.compiler();
+	
+	var result = compiler.compile('80');
+	
+	test.ok(result);
+	test.ok(Array.isArray(result));
+	test.equal(result.length, 1);
+	test.equal(result[0], 'Dup(1);');
+}
+
+exports['compile dup bytecodes'] = function (test) {
+	var compiler = compilers.compiler();
+	
+	for (var k = 0; k < 16; k++) {
+		var result = compiler.compile('8' + hexas[k]);
+		
+		test.ok(result);
+		test.ok(Array.isArray(result));
+		test.equal(result.length, 1);
+		test.equal(result[0], 'Dup(' + (k + 1) + ');');
+	}
+}
+
+exports['compile swap bytecodes'] = function (test) {
+	var compiler = compilers.compiler();
+	
+	for (var k = 0; k < 16; k++) {
+		var result = compiler.compile('9' + hexas[k]);
+		
+		test.ok(result);
+		test.ok(Array.isArray(result));
+		test.equal(result.length, 1);
+		test.equal(result[0], 'Swap(' + (k + 1) + ');');
+	}
+}
+
+exports['compile memory bytecodes'] = function (test) {
+	var compiler = compilers.compiler();
+
+	var result = compiler.compile('515253');
+	
+	test.ok(result);
+	test.ok(Array.isArray(result));
+	test.equal(result.length, 3);
+	test.equal(result[0], 'MLoad();');
+	test.equal(result[1], 'MStore();');
+	test.equal(result[2], 'MStore8();');
+}
+
+exports['compile storage bytecodes'] = function (test) {
+	var compiler = compilers.compiler();
+
+	var result = compiler.compile('5455');
+	
+	test.ok(result);
+	test.ok(Array.isArray(result));
+	test.equal(result.length, 2);
+	test.equal(result[0], 'SLoad();');
+	test.equal(result[1], 'SStore();');
+}
+
+exports['compile arithmetic operations bytecodes'] = function (test) {
+	var compiler = compilers.compiler();
+
+	var result = compiler.compile('0102030405060708090a0b');
+	
+	test.ok(result);
+	test.ok(Array.isArray(result));
+	test.equal(result.length, 11);
+	test.equal(result[0], 'Add();');
+	test.equal(result[1], 'Sub();');
+	test.equal(result[2], 'Mul();');
+	test.equal(result[3], 'Div();');
+	test.equal(result[4], 'Sdiv();');
+	test.equal(result[5], 'Mod();');
+	test.equal(result[6], 'SMod();');
+	test.equal(result[7], 'AddMod();');
+	test.equal(result[8], 'MulMod();');
+	test.equal(result[9], 'Exp();');
+	test.equal(result[10], 'SignExtend();');
+}
+
+
+
